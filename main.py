a17e6f6f1e2f0eff617c1675119e801af401648a
Ty <tyjen1218@gmail.com>
2025-08-31 13:11:20 -0400

    Added comments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 from numpy import genfromtxt, array
 
 def compute_error(b, m, points):
+
+    # Computing the mean squared error from the data points to the predicted regression line
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
@@ -20,15 +22,20 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
     return [b, m]
 
 def step_gradient(b_current, m_current, points, learning_rate):
+    
+    # Initializing the gradient at 0
     b_gradient = 0
     m_gradient = 0
     n = float(len(points))
+
+    # Calculating the gradient based on points
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
         b_gradient += -(2 / n) * (y -((m_current * x) + b_current))
         m_gradient += -(2 / n) * x * (y - ((m_current * x) + b_current))
 
+    # Assigning the new gradient for the next loop
     new_b = b_current - (learning_rate * b_gradient)
     new_m = m_current - (learning_rate * m_gradient)
 
@@ -36,7 +43,7 @@ def step_gradient(b_current, m_current, points, learning_rate):
 
 def run():
 
-    # Collection the data
+    # Collecting the data
     points = genfromtxt('data.csv', delimiter = ',')
 
     # Defining the rate of convergence for the model
